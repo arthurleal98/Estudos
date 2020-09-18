e766cbc6ca7cdf0cf010ed2348fc1b962aa63edb
arthurleal98 <arthurjfleal@gmail.com>
2020-09-18 15:52:26 -0300

    REcuperação dos arquivos

---
+++ Projeto3/js/Timer.js
@@ -0,0 +1,62 @@
+    var audio = new Audio("alarm.mp3")
+    var x;
+    var min = 00;
+    var seg = 00;
+    var countClick = 0;
+    var countClickStop=0;
+        function start(){
+            if(countClick==0){
+                countClick+=1;
+                countClickStop=0;
+
+                document.getElementById('start').innerHTML="Playing";
+                document.getElementById('pause').innerHTML="Pause";
+
+                x = setInterval(function(){
+                    
+                    document.getElementById('teste').innerHTML=("00"+min).slice(-2)+":"+("00"+seg).slice(-2);
+                    seg+=1;
+                    min +=Math.floor(seg/60);
+
+                    seg=seg%60;
+                    
+                    
+                    if(min==1){
+                        
+                        
+                        document.getElementById('teste').innerHTML=("00"+min).slice(-2)+":"+("00"+seg-1).slice(-2);
+                        document.getElementById('start').innerHTML="Start";                      
+
+                        
+                        clearInterval(x);
+                    }
+                },1000); 
+            }
+           
+            
+            
+        }
+        function pause(){
+            if(countClickStop==2){
+                document.getElementById('start').innerHTML="Start";
+                document.getElementById('pause').innerHTML="Pause";
+                min=0;
+                seg=0;
+                
+                document.getElementById('teste').innerHTML=("00"+min).slice(-2)+":"+("00"+seg).slice(-2);
+                clearInterval(x);
+
+            }
+            if(countClickStop==0){
+                countClick=0;
+                countClickStop+=1;
+                document.getElementById('start').innerHTML="Resume";
+            }
+            clearInterval(x);
+            if(countClickStop==1){
+                document.getElementById('pause').innerHTML="Stop";
+                countClickStop+=1;
+                clearInterval(x);
+            }
+            
+        }
